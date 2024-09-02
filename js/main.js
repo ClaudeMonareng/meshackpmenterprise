c32c466c6371e037c92e111d0584865304392e89
Lehlohonolo Monareng <lehlohonolomonareng@yahoo.com>
2024-09-02 14:51:53 +0200

    Latest changes

--- js/main.js
+++ js/main.js
@@ -127,6 +127,53 @@ var swiper = new Swiper(".clients-swiper", {
     },
 });
 
+// TYPEWRITER EFFECT
+
+const words= ['Boldness', 'Clarity', 'Originality', 'Precision']
+
+    let mainTimeline = gsap.timeline({
+        repeat: -1
+    })
+
+    // FOr each word, Create a new timeline, use the text plugin, then append that timeline to the main one
+
+    words.forEach(word => {
+        let textTimeline = gsap.timeline({
+            repeat: 1,
+            yoyo: true,
+            repeatDelay: 3
+        })
+
+        textTimeline.to('#typewriter', {
+            text: word,
+            duration: 1,
+            onUpdate: () => {
+                cursorTimeline.restart()
+                cursorTimeline.pause()
+            },
+            onComplete: () => {
+                cursorTimeline.play()
+            }
+        })
+
+        mainTimeline.add(textTimeline)
+    })
+
+    // Blinking cursor
+    let cursorTimeline = gsap.timeline({
+        repeat: -1,
+        repeatDelay: .8
+    })
+
+    cursorTimeline.to('#cursor', {
+        opacity: 1,
+        duration: 0
+    }).to('#cursor', {
+        opacity: 0,
+        duration: 0,
+        delay: .8
+    })
+
  // WEBSITE DARK/LIGHT THEME
 
 //  const themeBtn = document.querySelector(".theme-btn");
